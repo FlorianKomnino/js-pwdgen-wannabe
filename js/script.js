2a280b50f602adbb91820e5b88491a4f677707d8
FlorianKomnino <florian.komnino@outlook.it>
2022-11-18 15:41:45 +0100

    added rule to js to create the needed password

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ console.log('Hello World!')
 let nameRequest = prompt ('Type here Your name');
 let surnameRequest = prompt ('Type here Your surname');
 let colorRequest = prompt ('Type here Your favourite color');
-let mysticSecurityNumber = prompt ('22');
+let mysticSecurityNumber = '22';
+
+document.getElementById('generatedPassword').innerHTML = nameRequest + surnameRequest + colorRequest + mysticSecurityNumber;
