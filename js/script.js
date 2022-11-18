a2cd9e83966da8d99e93b52e69ddb9e289abc467
FlorianKomnino <florian.komnino@outlook.it>
2022-11-18 15:24:41 +0100

    added ID to 'p' element in html, added variables to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('Hello World!')
+
+let nameRequest = 'Type here Your name';
+let surnameRequest = 'Type here Your surname';
+let colorRequest = 'Type here Your favourite color';
+let mysticSecurityNumber = '22';
+
